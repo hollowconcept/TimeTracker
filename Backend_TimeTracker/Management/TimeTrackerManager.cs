8a25795ce7ddac7f86bb385d767e63e6195b66b3
luke.zwerger <144174419+hollowconcept@users.noreply.github.com>
2023-10-04 13:48:16 +0200

    Add files via upload

---
+++ Backend_TimeTracker/Management/TimeTrackerManager.cs
@@ -0,0 +1,24 @@
+﻿using Backend_TimeTracker.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_TimeTracker.Management
+{
+    public class TimeTrackerManager
+    {
+        public event OnErrorEventHandler OnError;
+        public delegate void OnErrorEventHandler(string message);
+
+        TimeRecordingDB TimeEntriesDB;
+
+        public TimeTrackerManager()
+        {
+            TimeEntriesDB = new TimeRecordingDB();
+        }
+
+      
+    }
+}
